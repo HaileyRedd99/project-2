0d6e92e61ec3b5c19f27bf208fd785f9e4ae40a6
Hailey Redd <rlm.hredd@gmail.com>
2020-10-25 16:29:05 -0400

    first change complete

--- content.js
+++ content.js
@@ -3,13 +3,13 @@ for (var i = 0; i < elements.length; i++) {
    var element = elements[i];
 
 
-//   console.log(element);
-//  element.style.setProperty('border', 'solid 1px red');
+  //console.log(element);
+ //element.style.setProperty('border', 'solid 1px red');
 
   var r = Math.floor(Math.random()*255);
   var g = Math.floor(Math.random()*255);
   var b = Math.floor(Math.random()*255);
-  var c = "rgb(" + r + "," + g + "," + b + ")"
+  var c = "rgb(" + r + "," + r + "," + r + ")"
 
   element.style.setProperty('background-color', c);
 

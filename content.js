1ea40335c612a3e0d3f3714db79da20931bcad3c
Hailey Redd <rlm.hredd@gmail.com>
2020-10-29 11:33:47 -0400

    txt replacements & grayscale filter

--- content.js
+++ content.js
@@ -15,9 +15,27 @@
 //
 // }
 var html = document.querySelector('html');
+//var elements = document.getElementsByTagName('html');
+html.style.setProperty('filter', 'grayscale(100%)'); 
+console.log(" *** in here *** ");
 var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT)
 
 var node;
 while (node = walker.nextNode()) {
-  node.nodeValue = node.nodeValue.replace(/theraputic/gi, 'abusive');
+  //node.nodeValue = node.nodeValue.replace(/ridge/gi,'dick');
+  node.nodeValue = node.nodeValue.replace(/therapeutic/gi,'abusive');
+  node.nodeValue = node.nodeValue.replace(/clinically/gi,' ');
+  node.nodeValue = node.nodeValue.replace(/approach/gi,'method');
+  node.nodeValue = node.nodeValue.replace(/a rigorous college prep curriculum/gi,'untrained staff members');
+  node.nodeValue = node.nodeValue.replace(/structured/gi,'an unstable');
+  node.nodeValue = node.nodeValue.replace(/tend to/gi,'degrade');
+  node.nodeValue = node.nodeValue.replace(/learning and connection/gi,'lies and manipulation');
+  node.nodeValue = node.nodeValue.replace(/work effectively with/gi,'permanently fuck up');
+  node.nodeValue = node.nodeValue.replace(/while attending to the underlying issues that create a complex presentation of behaviors and needs./gi,' ');
+  node.nodeValue = node.nodeValue.replace(/whom we help/gi,'we do not help');
+  node.nodeValue = node.nodeValue.replace(/about/gi,'lies');
+  node.nodeValue = node.nodeValue.replace(/why spring ridge/gi,'please god do not send your child here');
+  node.nodeValue = node.nodeValue.replace(/ /gi,' ');
+  node.nodeValue = node.nodeValue.replace(/ /gi,' ');
+  node.nodeValue = node.nodeValue.replace(/ /gi,' ');
 }
